4cbfa6b2cdca73386a61fac869214cb47a895734
StefanPDavis <snoodle22@hotmail.com>
2024-01-29 13:33:26 -0800

    added some starting values for seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,20 @@
+INSERT INTO department (name)
+VALUES ("Marketing"),
+       ("Sales"),
+       ("IT"),
+       ("HR"),
+       ("Security");
+
+INSERT INTO role (title, salary)
+VALUES ("Marketing Associate", "35,000"),
+       ("Sales Associate", "30,000 + commission"),
+       ("IT Technician", "40,000"),
+       ("HR associate", "40,000"),
+       ("Security Officer", "35,000");
+
+INSERT INTO employee (first_name, last_name)
+VALUES ("Elliot", "Smith"),
+       ("Amira", "Afzal"),
+       ("Christoper", "Lee"),
+       ("Verónica", "Rodriguez"),
+       ("Igor", "Ivanov");
