a48b2470f1a6c1fbce97541983bc69e5dac58329
StefanPDavis <snoodle22@hotmail.com>
2024-01-30 14:07:33 -0800

    changed salary strings to numbers, added department id, added more names and roles

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,16 +5,22 @@ VALUES ("Marketing"),
        ("HR"),
        ("Security");
 
-INSERT INTO role (title, salary)
-VALUES ("Marketing Associate", "35,000"),
-       ("Sales Associate", "30,000 + commission"),
-       ("IT Technician", "40,000"),
-       ("HR associate", "40,000"),
-       ("Security Officer", "35,000");
+INSERT INTO role (title, salary, department_id)
+VALUES ("Marketing Manager", 35000, 1),
+       ("Sales Representative", 30000, 2),
+       ("IT Technician", 40000, 3),
+       ("Director of IT", 90000, 3),
+       ("HR associate", 40000, 4),
+       ("Director of HR", 75000, 4),
+       ("Security Officer", 35000, 5),
+       ("Head of Security", 80000, 5);
 
 INSERT INTO employee (first_name, last_name)
 VALUES ("Elliot", "Smith"),
        ("Amira", "Afzal"),
        ("Christoper", "Lee"),
        ("Verónica", "Rodriguez"),
-       ("Igor", "Ivanov");
+       ("Igor", "Ivanov"),
+       ("John", "Doe"),
+       ("Bob", "Robertson"),
+       ("Steve", "Stevenson");
