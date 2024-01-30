688d4271fcf006f2b4b296b8f8231df350199eb4
StefanPDavis <snoodle22@hotmail.com>
2024-01-30 09:08:11 -0800

    moved primary key to id value in tables

--- db/schema.sql
+++ db/schema.sql
@@ -4,24 +4,21 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department (
-  id INT AUTO_INCREMENT,
-  name VARCHAR(30),
-  PRIMARY KEY(id)
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30)
 );
 
 CREATE TABLE role (
-  id INT AUTO_INCREMENT,
+  id INT AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30),
   salary DECIMAL,
-  department_id INT,
-  PRIMARY KEY(id)
+  department_id INT
 );
 
 CREATE TABLE employee (
-  id INT AUTO_INCREMENT,
+  id INT AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
   role_id INT,
-  manager_id INT,
-  PRIMARY KEY(id)
+  manager_id INT
 );
