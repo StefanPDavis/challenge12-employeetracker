575ada58f6358fe56ff12b1205720b16bbaa8de9
StefanPDavis <snoodle22@hotmail.com>
2024-01-30 14:32:27 -0800

    added requirements in server.js and express to package.json

---
+++ server.js
@@ -0,0 +1,12 @@
+const mysql = require("mysql2");
+const inquirer = require("inquirer");
+
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      user: 'root',
+      password: 'password',
+      database: 'employee_db'
+    },
+    console.log(`Connected to the employee_db database.`)
+  );
