4bf779dc362d8bd34c1c43e35fd3a75464d878d4
StefanPDavis <snoodle22@hotmail.com>
2024-01-30 14:11:42 -0800

    added not null to all table parameters

--- db/schema.sql
+++ db/schema.sql
@@ -4,21 +4,21 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30)
+  id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(30),
-  salary DECIMAL,
-  department_id INT
+  id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT NOT NULL
 );
 
 CREATE TABLE employee (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30),
-  last_name VARCHAR(30),
-  role_id INT,
-  manager_id INT
+  id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT NOT NULL
 );
