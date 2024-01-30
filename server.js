fb37e606e251986067be390ced5e41252d0a2841
StefanPDavis <snoodle22@hotmail.com>
2024-01-30 14:51:38 -0800

    added inquierer prompt

--- server.js
+++ server.js
@@ -12,3 +12,13 @@ const db = mysql.createConnection(
     },
     console.log(`Connected to the employee_db database.`)
   );
+
+  let editEmployeeList = function () {
+    inquirer.prompt([{
+        type: 'list',
+        name: 'prompt',
+        message: 'What would you like to do?',
+        choices: ['View All Department', 'View All Roles', 'View All Employees', 'Add A Department', 'Add A Role', 'Add An Employee', 'Update An Employee Role', 'Log Out']
+    }])
+
+  };
