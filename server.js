738cded27320a3a4ba0b9c9c2e61a52f965acca0
StefanPDavis <snoodle22@hotmail.com>
2024-01-30 15:00:19 -0800

    added answer for view all departments

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const mysql = require("mysql2");
 const inquirer = require("inquirer");
-const PORT = process.env.PORT || 3001;
-
 
 const db = mysql.createConnection(
     {
@@ -18,7 +16,16 @@ const db = mysql.createConnection(
         type: 'list',
         name: 'prompt',
         message: 'What would you like to do?',
-        choices: ['View All Department', 'View All Roles', 'View All Employees', 'Add A Department', 'Add A Role', 'Add An Employee', 'Update An Employee Role', 'Log Out']
-    }])
-
-  };
+        choices: ['View All Department', 'View All Roles', 'View All Employees', 'Add A Department', 'Add A Role', 'Add An Employee', 'Update An Employee Role']
+    }]
+    ).then((answers) => {
+        if (answers.prompt === 'View All Department') {
+        db.query(`SELECT * FROM department`, (err, result) => {
+            if (err) throw err;
+            console.log("Viewing All Departments: ");
+            console.table(result);
+            editEmployeeList();
+        });
+    }
+    })
+};
