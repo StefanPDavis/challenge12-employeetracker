4802c5fb6505baa9a5faed44e3071b826c31cd58
StefanPDavis <snoodle22@hotmail.com>
2024-01-30 16:28:03 -0800

    reduced seed values so we have something to add when running js

--- db/seeds.sql
+++ db/seeds.sql
@@ -6,14 +6,11 @@ VALUES ("Marketing"),
        ("Security");
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("Marketing Manager", 35000, 1),
+VALUES ("Marketing Assocaiate", 35000, 1),
        ("Sales Representative", 30000, 2),
        ("IT Technician", 40000, 3),
-       ("Director of IT", 90000, 3),
-       ("HR associate", 40000, 4),
-       ("Director of HR", 75000, 4),
+       ("HR Representative", 40000, 4),
        ("Security Officer", 35000, 5),
-       ("Head of Security", 80000, 5);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ("Elliot", "Smith", 1, NULL),
@@ -21,6 +18,3 @@ VALUES ("Elliot", "Smith", 1, NULL),
        ("Christoper", "Lee", 3, 2),
        ("Veronica", "Rodriguez", 4, NULL),
        ("Igor", "Ivanov", 5, 3),
-       ("John", "Doe", 6, NULL),
-       ("Bob", "Robertson", 7, 4),
-       ("Steve", "Stevenson", 8, NULL);
